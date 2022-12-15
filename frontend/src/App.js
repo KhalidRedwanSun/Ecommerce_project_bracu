e1c159eb72c8395ac89b9f99887a7b9958c3ba39
KhalidRedwanSun <khalidredwan701@gmail.com>
2022-12-15 20:22:43 +0600

    frontend_first_part

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+      <h1> </h1>
+    </div>
+  );
+}
+
+export default App;
