0f5d75d4f65f6f90f04789a3918c2db1d935ee07
KhalidRedwanSun <khalidredwan701@gmail.com>
2022-12-18 00:13:08 +0600

    React_Router_add_and_redux

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,6 +9,7 @@ const {
   deleteProduct,
   createProductReview,
   getProductReviews,
+  deleteReview,
 } = require("../controllers/productController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
@@ -32,5 +33,7 @@ router
 router.route("/review").put(isAuthenticatedUser, createProductReview);
 
 router.route("/reviews").get(isAuthenticatedUser, getProductReviews);
+
+router.route("/reviews").delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
+import MetaData from "./layout/MetaData";
 
 const Home = () => {
   return (
-    <div className="container container-fluid">
+    <Fragment>
+      <MetaData title={"Buy Best Products Online"} />
       <h1 id="products_heading">Latest Products</h1>
 
       <section id="products" className="container mt-5">
@@ -16,7 +18,9 @@ const Home = () => {
               />
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">
-                  <a href="">128GB Solid Storage Memory card - SanDisk Ultra</a>
+                  <a href="https://www.ryanscomputers.com/sandisk-ultra-sdun4-128gb-sdxc-uhs-i-class-10-memory-card">
+                    128GB Solid Storage Memory card - SanDisk Ultra
+                  </a>
                 </h5>
                 <div className="ratings mt-auto">
                   <div className="rating-outer">
@@ -25,7 +29,11 @@ const Home = () => {
                   <span id="no_of_reviews">(5 Reviews)</span>
                 </div>
                 <p className="card-text">$45.67</p>
-                <a href="#" id="view_btn" className="btn btn-block">
+                <a
+                  href="https://www.ryanscomputers.com/sandisk-ultra-sdun4-128gb-sdxc-uhs-i-class-10-memory-card"
+                  id="view_btn"
+                  className="btn btn-block"
+                >
                   View Details
                 </a>
               </div>
@@ -33,7 +41,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-    </div>
+    </Fragment>
   );
 };
 
